cbf4d8bf2d09861756e5f09e5c8f4ef81596e826
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-19 21:13:09 -0400

    app structure

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    message = 'Hello World'
+    return render_template('index.html',message=message)
+
+@app.route('/pitch/<int:pitch_id>')
+def pitch(pitch_id):
+
+    '''
+    View pitch page function that returns the pitch details page and its data
+    '''
+    return render_template('pitch.html',id = pitch_id)
